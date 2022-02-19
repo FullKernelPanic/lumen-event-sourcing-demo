5274ac42d15785c3b46b886f3156ba15bd681a83
FullKernelPanic <zackraven@gmail.com>
2022-02-19 10:47:45 +0100

    fix: adding migrations and seeders

---
+++ database/migrations/2022_02_18_101039_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id')->default(1);
+            $table->string('product_name')->default('item 1');
+            $table->decimal('quantity')->default(1);
+            $table->decimal('total_price')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
